3c38b15820425daecdc4bf4c7578de61d4087dad
VanDanh1808 <danhskipper18@gmail.com>
2025-12-23 04:38:45 +0700

    Refactor HTML files to use local Bootstrap and Iconify resources

---
+++ js/utils/icons.js
@@ -0,0 +1,89 @@
+/**
+ * SVG Icon System - Replaces Iconify with local SVG sprites
+ * Load this script to convert all .iconify spans to inline SVGs
+ */
+(function () {
+  // Icon mapping from Iconify data-icon to local symbol IDs
+  const iconMap = {
+    "iconoir:search": "icon-search",
+    "mdi:facebook": "icon-facebook",
+    "mdi:youtube": "icon-youtube",
+    "ic:baseline-tiktok": "icon-tiktok",
+    "mdi:email": "icon-email",
+    "simple-icons:zalo": "icon-zalo",
+    "mdi:home": "icon-home",
+    "mdi:arrow-left": "icon-arrow-left",
+    "mdi:calendar": "icon-calendar",
+    "mdi:bookmark": "icon-bookmark",
+    "mdi:currency-usd": "icon-currency-usd",
+    "mdi:map-marker": "icon-map-marker",
+    "ic:outline-email": "icon-outline-email",
+    "ic:outline-phone": "icon-outline-phone",
+    "mdi:twitter": "icon-twitter",
+    "mdi:linkedin": "icon-linkedin",
+    "mdi:phone": "icon-phone",
+  };
+
+  // Load SVG sprite into document
+  function loadSvgSprite() {
+    return fetch("/assets/svg/icons.svg")
+      .then((response) => response.text())
+      .then((svgContent) => {
+        const div = document.createElement("div");
+        div.style.display = "none";
+        div.innerHTML = svgContent;
+        document.body.insertBefore(div, document.body.firstChild);
+      });
+  }
+
+  // Replace all iconify spans with inline SVGs
+  function replaceIcons() {
+    const iconElements = document.querySelectorAll(".iconify[data-icon]");
+
+    iconElements.forEach((el) => {
+      const iconName = el.getAttribute("data-icon");
+      const symbolId = iconMap[iconName];
+
+      if (symbolId) {
+        const width = el.getAttribute("data-width") || "24";
+        const height = el.getAttribute("data-height") || "24";
+
+        const svg = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "svg"
+        );
+        svg.setAttribute("width", width);
+        svg.setAttribute("height", height);
+        svg.setAttribute("fill", "currentColor");
+        svg.setAttribute("aria-hidden", "true");
+
+        // Copy classes from original element
+        if (el.className) {
+          svg.setAttribute("class", el.className.replace("iconify", "").trim());
+        }
+
+        const use = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "use"
+        );
+        use.setAttributeNS(
+          "http://www.w3.org/1999/xlink",
+          "xlink:href",
+          "#" + symbolId
+        );
+        svg.appendChild(use);
+
+        el.parentNode.replaceChild(svg, el);
+      }
+    });
+  }
+
+  // Initialize when DOM is ready
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", function () {
+      loadSvgSprite().then(replaceIcons);
+    });
+  } else {
+    loadSvgSprite().then(replaceIcons);
+  }
+})();
